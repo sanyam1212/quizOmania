e46e878651c80f49db1bf74253fec145d27b0dea
abhishekbagria <abhishekbagria1994@gmail.com>
2020-09-08 00:24:09 +0530

    Commit-3 // spring-boot-starter-common-2.2.2.RELEASE.jar

---
+++ spring-boot-starter-common/install.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=spring-boot-starter-common-2.2.2.RELEASE.jar -DgroupId=org.springframework.boot -DartifactId=spring-boot-starter-common -Dversion=2.2.2.RELEASE -Dpackaging=jar
